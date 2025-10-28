016cbd53d3ba6699abf6bac6cb7e0786d3288c09
MBryder <mbryde22@student.aau.dk>
2025-10-28 10:27:00 +0100

    docs

--- frontend/src/views/DocsView.vue
+++ frontend/src/views/DocsView.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="flex flex-row">
-    <div class="flex">
+  <div class="flex flex-row h-screen w-screen bg-surface-light">
+    <div class="flex flex-col w-[250px] bg-surface-light-secondary border-r border-border-dark">
+      <div class="h-[60px] w-full">
+        <div></div>
+      </div>
+    </div>
+    <div class="flex flex-col h-full w-full">
+      <div class="flex flex-row h-[60px] w-full border-b-2 border-accent"></div>
+      <div class="flex flex-col h-full w-full justify-center items-center">
+        <div class="flex flex-row h-[50px] w-[1200px] border-b border-b-border-light-subtle"></div>
+        <div class="h-full w-[1000px] bg-white border-x border-x-border-light-subtle p-12">
+          <textarea name="" id="" class="w-full h-full focus:outline-none justify-start items-start"></textarea>
+        </div>
 
+      </div>
     </div>
   </div>
 </template>
